5fee25027977c8f539681e70e2f1de55089efad2
Asem Aly <eng.asem.aly@hotmail.com>
2024-07-01 06:34:03 +0300

    AddModals

---
+++ src/app/mods/modal/modal.component.ts
@@ -0,0 +1,27 @@
+import { Component, ElementRef, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './modal.component.html',
+  styleUrl: './modal.component.css'
+})
+export class ModalComponent {
+  @Output() close = new EventEmitter();
+  constructor(private el:ElementRef) {
+
+  
+  }
+
+  ngOnInit(){
+    document.body.appendChild(this.el.nativeElement);
+  }
+
+  ngOnDestroy(){
+    this.el.nativeElement.remove();
+  }
+  onCloseClick(){
+    this.close.emit();
+  }
+}
---
+++ src/app/mods/mods.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mods',
+  standalone: true,
+  imports: [],
+  templateUrl: './mods.component.html',
+  styleUrl: './mods.component.css'
+})
+export class ModsComponent {
+
+}
